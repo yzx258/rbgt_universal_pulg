a88e0642d9331977d9c8a54bf060d9cfacda1202
俞春旺 <yucw@itsmycar.cn>
2022-04-06 15:16:05 +0800

    团队通用组件 - 调整代码

--- rbgt_rabbitmq/src/main/java/com/rbgt/rabbitmq/controller/RabbitmqController.java
+++ rbgt_rabbitmq/src/main/java/com/rbgt/rabbitmq/controller/RabbitmqController.java
@@ -2,6 +2,8 @@ package com.rbgt.rabbitmq.controller;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
 import org.springframework.amqp.AmqpException;
@@ -14,10 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.rabbitmq.client.BuiltinExchangeType;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.*;
 import com.rbgt.rabbitmq.example.direct.RabbitDirectConfig;
 import com.rbgt.rabbitmq.example.fanout.RabbitFanoutConfig;
 import com.rbgt.rabbitmq.example.topic.RabbitTopicConfig;
@@ -113,9 +112,14 @@ public class RabbitmqController {
             connection = factory.newConnection();
             channel = connection.createChannel();
             // 创建交换机
-            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.FANOUT, true, true, null);
+            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.FANOUT, true, false, null);
             // 开启发送方确认模式
             channel.confirmSelect();
+            // 创建队列
+            channel.queueDeclare("log_fanout_queue1", false, false, false, null);
+            // 绑定队列
+            channel.queueBind("log_fanout_queue1", EXCHANGE_NAME, "");
+
             // 发送消息
             String message = "[日志][支付信息][订单号:C20001525][支付ID:10]";
             channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes(StandardCharsets.UTF_8));
@@ -130,12 +134,57 @@ public class RabbitmqController {
                 // 释放资源
                 channel.close();
             }
+        }
+        return "SEND CHANNEL MESSAGE SUCCESS";
+    }
 
-            if (null != connection && connection.isOpen()) {
+    @GetMapping("/channelYc")
+    public String channelYc() throws IOException, TimeoutException {
+        final String EXCHANGE_NAME = "logs-yc";
+        Connection connection = null;
+        Channel channel = null;
+        try {
+            ConnectionFactory factory = new ConnectionFactory();
+            // 设置参数
+            factory.setHost("192.168.30.210");
+            factory.setPort(5672);
+            factory.setUsername("yiautos");
+            factory.setPassword("yiautos123456");
+
+            connection = factory.newConnection();
+            channel = connection.createChannel();
+            // 创建交换机
+            Map<String, Object> arguments = new LinkedHashMap<>();
+            arguments.put("x-delayed-type", "fanout");
+            channel.exchangeDeclare(EXCHANGE_NAME, "x-delayed-message", true, false, arguments);
+            // 开启发送方确认模式
+            channel.confirmSelect();
+
+            // 创建队列
+            channel.queueDeclare("log_fanout_queue1", false, false, false, null);
+            // 绑定队列
+            channel.queueBind("log_fanout_queue1", EXCHANGE_NAME, "");
+
+            // 发送消息
+            String message = "[日志][延迟消息][支付信息][订单号:C20001525][支付ID:10]";
+
+            Map<String, Object> headers = new LinkedHashMap<>();
+            headers.put("x-delay", "6000");
+            AMQP.BasicProperties.Builder builder = new AMQP.BasicProperties.Builder();
+            builder.headers(headers);
+            AMQP.BasicProperties props = builder.build();
+            channel.basicPublish(EXCHANGE_NAME, "", props, message.getBytes(StandardCharsets.UTF_8));
+            // 回调机制
+            boolean result = channel.waitForConfirms();
+            System.out.println("是否响应成功 - result：" + result);
+        } catch (IOException | TimeoutException | InterruptedException e) {
+            e.fillInStackTrace();
+        } finally {
+
+            if (null != channel && channel.isOpen()) {
                 // 释放资源
-                connection.close();
+                channel.close();
             }
-
         }
         return "SEND CHANNEL MESSAGE SUCCESS";
     }
